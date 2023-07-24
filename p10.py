18078bb7cd12cbdaa2973c2de9439d8fec43f255
sina <sinasohrab84@gmail.com>
2023-07-24 12:48:11 +0330

    end

--- p10.py
+++ p10.py
@@ -1,11 +1,15 @@
+numbers = int(input("your number: "))
+
+
 def is_prime(n):
     for i in range(0, n):
         if n % 3 == 0 or n % 5 == 0:
             return True
     return False
 
-sum=0
-for i in range(0, 10):
+
+sum = 0
+for i in range(0, numbers):
     if is_prime(i):
-        sum=i+sum
+        sum = i + sum
 print(sum)
