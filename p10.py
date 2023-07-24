2374723dfc4ac5b4aa4e23dc46f478036462aa2e
sina <sinasohrab84@gmail.com>
2023-07-24 12:47:53 +0330

    end

---
+++ p10.py
@@ -0,0 +1,11 @@
+def is_prime(n):
+    for i in range(0, n):
+        if n % 3 == 0 or n % 5 == 0:
+            return True
+    return False
+
+sum=0
+for i in range(0, 10):
+    if is_prime(i):
+        sum=i+sum
+print(sum)
