437e65e37b3133b00c9bee43de026c673ec96c49
sina <sinasohrab84@gmail.com>
2023-07-25 11:09:37 +0330

    create p12

--- p11.py
+++ p11.py
@@ -2,7 +2,7 @@ number = 1000000
 
 
 def is_prim(n):
-    for i in range(2,n):
+    for i in range(2, int(n**0.5) + 1):
         if n % i == 0 and n % 1 == 0:
             return False
     return True
