3dba496e04ef37d775c86d3c42e2ee1cc9beeefa
sina <sinasohrab84@gmail.com>
2023-07-28 13:26:23 +0330

    fixe p13 and create def_in_def project

---
+++ def_in_def.py
@@ -0,0 +1,11 @@
+def number(n):
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+for i in range(2, 1000000):
+    if number(i):
+        numberr = i
+print(numberr)
