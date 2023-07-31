d608563e51096826891fba18ec38ba07962686d2
sina <sinasohrab84@gmail.com>
2023-07-31 22:13:02 +0330

    Second attempt

--- p14.py
+++ p14.py
@@ -3,9 +3,9 @@ list_age=[]
 list_city=[]
 
 while True:
-    name=str(input("whats your name: "))
-    age=int(input("age: "))
-    city=str(input("What is the name of fame: "))
+    name=(input("whats your name: "))
+    age=(input("age: "))
+    city=(input("What is the name of fame: "))
     if name=="0":
         break
     list_name.append(name)
