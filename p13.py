e489c66fb6177199913d437953d372e0c8d748f7
sina <sinasohrab84@gmail.com>
2023-07-31 21:32:46 +0330

    end

--- p13.py
+++ p13.py
@@ -1,13 +1,15 @@
-number=int(input("your number: "))
+number = int(input("your number: "))
+Even = []
+
 
 def Even_number(n):
-    for i in range(0,n):
-        if n%2==0:
-            return True
-        else:
-            return False
-Even=[]
-for i in range(0,number):
+    for i in range(0, n):
+        if n % 2 == 0:
+            return True    
+    return False
+
+
+for i in range(0, number):
     if Even_number(i):
         Even.append(i)
 
