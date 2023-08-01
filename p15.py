b69cba77179732e82009ee4ff834c5d15a765f22
sina <sinasohrab84@gmail.com>
2023-08-01 14:17:29 +0330

    end p15

---
+++ p15.py
@@ -0,0 +1,8 @@
+for i in range(0, 100):
+    print(i)
+    b_or_d = input("bozorg tae [y], doroste [d]: ")
+    if b_or_d == "b":
+        print
+    if b_or_d=="d":
+        print("your number= ",i)
+        break
