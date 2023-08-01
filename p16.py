dc1353f429c920031f95818d690b7a663c8bb16b
sina <sinasohrab84@gmail.com>
2023-08-01 14:33:47 +0330

    end project p16

--- p16.py
+++ p16.py
@@ -1,10 +1,8 @@
 number = int(input("your number: "))
-def zarb(n):
-    for i in range(1,n):
-        j_zarb=2*i
-        return j_zarb
+zarb = int(input("your zarb: "))
+zarb += 1
 
-for i in range(1,11):
-    if zarb(i):
-        javab=number*i
-        print(number,"*",i,"=",javab)
+
+for i in range(1, zarb):
+    javab = number * i
+    print(number, "*", i, "=", javab)
