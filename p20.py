6c3a19dba1c0a130327f9c48b70bad315f614b94
sina <sinasohrab84@gmail.com>
2023-08-03 23:40:45 +0330

    developing

--- p20.py
+++ p20.py
@@ -1,3 +1,14 @@
 import random
-number=random.randint(1,99)
 
+number = random.randint(1, 99)
+print(number)
+javab = input("[b], [k], [d]: ")
+
+while number != "d":
+    print(number)
+    javab = input("[b], [k], [d]: ")
+    if javab == "b":
+        number = random.randint(number, 99)
+    if javab == "k":
+        number = random.randint(1, number)
+print("wow")
