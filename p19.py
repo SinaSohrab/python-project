869157069103febdd0f71db7495cb5f8c1c2f52d
sina <sinasohrab84@gmail.com>
2023-08-03 13:14:50 +0330

    fixe p19

--- p19.py
+++ p19.py
@@ -1,22 +1,21 @@
 def ramz_is_okey(ramz_digits):
     if (
         ramz_digits["yek"] == ramz_digits["panj"]
-        and (ramz_digits["yek"] * 2) == ramz_digits["do"] + 1
+        and (ramz_digits["yek"] * 2)+1 == ramz_digits["do"]
         and (ramz_digits["panj"] / 2) == ramz_digits["chahar"]
-        and ramz_digits["shish"] - 1 == ramz_digits["yek"]
-        and (ramz_digits["se"] * 4)
+        and (ramz_digits["yek"] - 1) == ramz_digits["shish"]
+        and (ramz_digits["se"] * 4)+1
         == ramz_digits["yek"]
         + ramz_digits["do"]
         + ramz_digits["se"]
         + ramz_digits["chahar"]
         + ramz_digits["panj"]
         + ramz_digits["shish"]
-        - 1
     ):
         return True
 
 
-for ramz in range(100000, 10000000):
+for ramz in range(0, 1000000):
     this_ramz = str(ramz).zfill(6)
 
     ramz_digits = {}
