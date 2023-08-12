6c658138a75271f5bcad54f0ea094a442c0bff63
sina <sinasohrab84@gmail.com>
2023-08-12 18:58:32 +0330

    developing p23

--- p23.py
+++ p23.py
@@ -1,20 +1,25 @@
 users = []
+gmail=[]
 while True:
     user_name = str(input("user name: "))
     email_addres = str(input("email addres: "))
     password = int(input("your password: "))
     user = user_name, email_addres, password
     users.append(user)
+    gmail.append(email_addres)
     exit = input("Do you want to continue?[y]|[n]: ")
     if exit == "n":
         break
 
 
-def chek_gmail(email_addres):
-    if "@gami" in email_addres:
-        return print("isGmail= YES")
+def chek_gmail(gmail):
+    if "@gmail" in gmail:
+        return print("users",users, "isGmail= YES")
     else:
-        return print("isGmail= NO")
+        return print("users",users, "isGmail= NO")
+    
+for i in gmail:
+    if chek_gmail(i):
+        gmail=i
 
-
-print(users, chek_gmail(email_addres))
+        print(users)
