002867d6cb54ea3db24686a8b26a8497310e09e2
sina <sinasohrab84@gmail.com>
2023-08-23 12:30:17 +0330

    fixe project

--- p28.py
+++ p28.py
@@ -1,4 +1,4 @@
-the_length=input("length of the rectangle: ")
-width=input("the width of the rectangel: ")
-answer=the_length*width
+the_length = input("length of the rectangle: ")
+width = input("the width of the rectangel: ")
+answer = the_length * width
 print(answer)
