1ea417b19733973c78b76739e6ac7727cef07444
sina <sinasohrab84@gmail.com>
2023-08-23 12:31:14 +0330

    dibuge

--- p28.py
+++ p28.py
@@ -1,4 +1,4 @@
-the_length = input("length of the rectangle: ")
-width = input("the width of the rectangel: ")
+the_length = float(input("length of the rectangle: "))
+width = float(input("the width of the rectangel: "))
 answer = the_length * width
 print(answer)
