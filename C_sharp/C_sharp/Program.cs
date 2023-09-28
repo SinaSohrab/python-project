6b1bef136dc938c803a14951adaf34fb31b53054
SinaSohrab <sinasohrab84@gmail.com>
2023-09-28 15:50:55 +0330

    end project

--- C_sharp/C_sharp/Program.cs
+++ C_sharp/C_sharp/Program.cs
@@ -10,6 +10,13 @@ namespace C_sharp
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 11; i++)
+            {
+                if (i%2!=0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
