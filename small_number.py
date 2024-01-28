d7977f08c389c0ac9ae3f75df9ab2481e538788f
SinaSohrab <sinasohrab84@gmail.com>
2024-01-28 17:24:37 +0330

    name change

---
+++ small_number.py
@@ -0,0 +1,11 @@
+sum = 0
+i = 0
+while True:
+    number = float(input("your number: "))
+    if number == 0:
+        break
+    i += 1
+    sum = number + sum
+    
+    
+print(avrage := sum / i)
