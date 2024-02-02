ce3aaf99334dfd34e42dea2f48f80f5895cef067
Sina <sinasohrab84@gmail.com>
2024-02-02 14:43:25 +0330

    save hop_game.py project

--- hop_game.py
+++ hop_game.py
@@ -3,17 +3,15 @@ number += 1
 sum = 0
 
 for i in range(1, number):
-    your_number = ("number: ")
+    sum+=1
+    
+    
+    your_number = int(input("number: "))
 
     if your_number != i:
         print("loser")
         break
 
-    sum += 1
-    print("sum: ", sum)
-
-    if sum % 5 == 0:
-        print("sum")
 
     if i % 5 == 0:
         print("i")
