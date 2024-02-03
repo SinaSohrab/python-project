7f185f4395f48e472e7ea0e8db6bbfc0b37e0569
Sina <sinasohrab84@gmail.com>
2024-02-03 21:52:26 +0330

    save BMI.py file and add # in code

--- BMI.py
+++ BMI.py
@@ -1,6 +1,8 @@
-height = float(input("input your height: "))
-weight = float(input("input your weight: "))
+height = float(input("input your height: for cm: "))
+weight = float(input("input your weight: for k: "))
+#tabdil sc be m
 height /= 100
+# 1,   argoman round ro 1 adad ashar ro barmogardone
 BMI = round(weight / (height * height), 1)
 
 print(f"BMI: {BMI}")
